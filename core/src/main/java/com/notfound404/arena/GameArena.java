305c06c85f0a80c18148a4e23f5fd124be528d4d
LuoYanHao-2025 <fredlyh.dcred@gmail.com>
2026-01-05 21:50:44 +0800

    initial the project

---
+++ core/src/main/java/com/notfound404/arena/GameArena.java
@@ -0,0 +1,20 @@
+package com.notfound404.arena;
+
+public class GameArena {
+    
+    //The cells of the arena
+    private final static int ARENA_WIDTH = 40;
+    private final static int ARENA_HEIGHT = 40;
+
+    /*The grid representing the arena
+    * 0 = empty cell
+    * 1 = jetwall cell
+    * 2 = bike cell
+    * 3 = accelerator cell
+    * 4 = wall cell
+    * 5 = disco cell
+    * */
+
+    private int[][] grid;
+
+}
---
+++ core/src/main/java/com/notfound404/character/Bike.java
@@ -0,0 +1,9 @@
+package com.notfound404.character;
+
+public class Bike {
+    
+}
+
+class disco {
+    
+}
